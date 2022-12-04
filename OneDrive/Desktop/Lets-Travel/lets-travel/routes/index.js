f9f6d03e67dba69bb471464abad8e63cbd02478b
FlyingFox007 <1rn20cs042@rnsit.onmicrosoft.com>
2022-12-05 02:53:29 +0530

    1

---
+++ OneDrive/Desktop/Lets-Travel/lets-travel/routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+// require controllers:
+// const hotelController= require('../controllers/hotelController')
+const hotelController=require('../controllers/hotelController');
+
+
+/* GET home page. */
+router.get('/',hotelController.homePage);
+
+router.get('/all', hotelController.listAllHotels);
+
+
+module.exports = router;
